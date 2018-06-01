cff3d4f9339ac994dde278f36aecb0f837b15ad3
fanlei <fanlei@hansap.com>
2018-06-01 11:10:45 +0800

    网络框架的bug修改

--- library/src/main/java/com/fancy/library/net/BaseCallBack.java
+++ library/src/main/java/com/fancy/library/net/BaseCallBack.java
@@ -8,7 +8,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- * 请求回调
+ * 请求
  *
  * @author fanlei
  * @version 1.0 2018\5\31 0031
@@ -16,9 +16,9 @@ import retrofit2.Response;
  */
 public abstract class BaseCallBack<T> implements Callback<T> {
 
-    private final int defaultFailCode = -1;
+    protected final int defaultFailCode = -1;
 
-    private final Context mContext;
+    protected final Context mContext;
 
     public BaseCallBack(@NonNull Context context) {
         this.mContext = context;
@@ -30,18 +30,18 @@ public abstract class BaseCallBack<T> implements Callback<T> {
         if (response.raw().code() == 200) {
             onSuc(response);
         } else {//失败响应
-            onFail(Error.buildError(response).getErrorMessage(), response.raw().code());
+            onFail(response, Error.buildError(response).getErrorMessage(), response.raw().code());
         }
 
     }
 
     @Override
     public void onFailure(Call<T> call, Throwable t) {//网络问题会走该回调
-        onFail(Error.buildError(t).getErrorMessage(), defaultFailCode);
+        onFail(null, Error.buildError(t).getErrorMessage(), defaultFailCode);
     }
 
     public abstract void onSuc(Response<T> response);
 
-    public abstract void onFail(String message, int failCode);
+    public abstract void onFail(Response<T> response, String message, int failCode);
 
 }
